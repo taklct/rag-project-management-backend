4a8239e0872fa76fcbfd985ff7131b7c1f73a413
taklct <50726132+taklct@users.noreply.github.com>
2025-11-08 13:15:07 +0800

    Update JIRA API token in config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ EMBEDDING_DEPLOYMENT_NAME="text-embedding-3-large"
 JIRA_ENDPOINT_URL="https://origat.atlassian.net"
 JIRA_API_URL=f"{JIRA_ENDPOINT_URL}/rest/api/3"
 JIRA_USERNAME="taklct113@gmail.com"
-JIRA_API_TOKEN="ATATT3xFfGF0Ce7-Az_Xb81T5M47fU2Z3wNKkNaawjbjBaF9rkltnW_sqSvYibhXWTW2_onOpQYoveyFkh_3aszvfFajy3EcK1MDyAxK6l_1nQDLxAHCa9-HTao9QJ4zkrPG5OWKrHO8A_5iBBnMHSxUTsYH4jrxeUhOIywuz0vZKnmn97oqo9E=70439450"
+JIRA_API_TOKEN="ATATT3xFfGF0a9IP9-QTrvHFp2BjSrkWwfG_nl8nYc6-E8feijBpnK0iViSdUwQPK5QfMi3_YZyitYeCVA_dYa0SGHWMG0IOedz9OwYR5al1QHqsZj_jfFkzCJ_KOyBYttE__4EJ9RVwNMNWhy7pkwa3ItnI5Cwuholw_PXoTiuutxRlo8jUfys=1BB83DD4"
 # Optional override for Azure OpenAI API version
 API_VERSION = "2025-01-01-preview"
 
